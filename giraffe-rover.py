9772ce16fc1222b1dbd3e7d5535078becf016b5e
Jaison Miller <jaison.miller@gmail.com>
2018-01-16 15:50:35 +0000

    Initial Upload
    Giraffe Rover files, basic (slightly unworking) interface.

---
+++ giraffe-rover.py
@@ -0,0 +1,235 @@
+# This is a rework of the RoboArm - Simple interface, allowing the motors to be run at the same time, and fully
+# adopting the correct API, allowing for other operators to use it correctly, without sending invalid commands to the system.
+
+# OWI-535 Robotic Arm - Advanced Web Interface and Controller / Python + Bottle
+
+# imports
+from bottle import Bottle, run, template, request, static_file
+import usb.core, usb.util
+
+def motormsk (motor_id, motor_config):
+  return (motor_config[motor_id][0][0] | motor_config[motor_id][1][0], motor_config[motor_id][0][1] | motor_config[motor_id][1][1], motor_config[motor_id][0][2] | motor_config[motor_id][1][2])
+  
+def motorcmb (motor_id_1, motor_dir_1, motor_id_2, motor_dir_2, motor_config):
+  return (motor_config[motor_id_1][motor_dir_1][0] | motor_config[motor_id_2][motor_dir_2][0], motor_config[motor_id_1][motor_dir_1][1] | motor_config[motor_id_2][motor_dir_2][1], motor_config[motor_id_1][motor_dir_1][2] | motor_config[motor_id_2][motor_dir_2][2])
+
+# MOTORS: all, m1, m2, m3, m4, m5 (+, -)
+MOTORS = (
+  (
+    (0b01010101, 0b00000001, 0b00000000),
+    (0b10101010, 0b00000010, 0b00000000)
+  ),
+  (
+    (2**0, 0, 0),
+    (2**1, 0, 0)
+  ),
+  (
+    (2**2, 0, 0),
+    (2**3, 0, 0)
+  ),
+  (
+    (2**4, 0, 0),
+    (2**5, 0, 0)
+  ),
+  (
+    (2**7, 0, 0),
+    (2**6, 0, 0)
+  ),
+  (
+    (0, 2**0, 0),
+    (0, 2**1, 0)
+  )
+)
+
+LED = ((0,0,1),(0,0,0))
+
+# TRACKS: both, right, left (forward, reverse) 
+TRACKS = (
+    (
+      motorcmb(4, 0, 5, 0, MOTORS),
+      motorcmb(4, 1, 5, 1, MOTORS)
+    ),
+    MOTORS[4],
+    MOTORS[5]
+  )
+
+# ARM: all, elbow, wrist, grip (forward/open, reverse/close)  
+ARM = (
+  (
+      motorcmb(0, 0, 1, 0,((motorcmb(1, 0, 2, 0, MOTORS),motorcmb(1, 1, 2, 1, MOTORS)),(MOTORS[3]))),
+      motorcmb(0, 1, 1, 1,((motorcmb(1, 0, 2, 0, MOTORS),motorcmb(1, 1, 2, 1, MOTORS)),(MOTORS[3])))
+    ),
+    MOTORS[3],
+    MOTORS[2],
+    MOTORS[1]
+  )
+
+OWI535USB = {
+  "description": "OWI-535 Robotic Arm - USB Interface",
+  "identification":{
+    "description":"These are the parameters used to identify the RoboArm using the USB connection",
+    "idVendor": 0x1267,
+    "idProduct": 0x000
+  },
+  "initialisation":{
+    "description": "These variables are used to transfer data on the created USB connection",
+    "bmRequestType": 0x40,
+    "bmRequest": 6,
+    "wValue": 0x100,
+    "wIndex": 0
+  }
+}
+
+ROVER = {
+  "components": {
+    "tracks": {
+      "description": "The <ARM> components are mounted on this rotating tracked system",
+      "mask": motormsk(0, TRACKS),
+      "features": {
+        "both": {
+          "description": "This is a pseudo-feature that allows for motor controls for the tracks - it should cover all the needs for the tracks as running them individually isn't efficient",
+          "mask": motormsk(0, TRACKS),
+          "verbs": {
+            "stop": motormsk(0, TRACKS),
+            "forward": TRACKS[0][0],
+            "reverse": TRACKS[0][1],
+            "turncw": motorcmb(1, 0, 2, 1, TRACKS),
+            "turnccw": motorcmb(1, 1, 2, 0, TRACKS)
+          }
+        }
+      }
+    },
+    "arm": {
+      "description": "This component is mounted on the <BASE>, and replicates a typical human arm - with features, i.e. shoulder, elbow, wrist and a gripper.",
+      "mask": motormsk(0, ARM),
+      "features": {
+        "all": {
+          "description": "This is a pseudo-feature that allows for complex functions to be predescribed for the whole <ARM> component",
+          "mask": motormsk(0, ARM),
+          "verbs": {
+            "stop": motormsk(0, ARM)
+          }
+        },
+        "elbow": {
+          "description":"",
+          "mask": motormsk(1, ARM),
+          "verbs": {
+            "open": ARM[1][0],
+            "close": ARM[1][1],
+            "stop": motormsk(1, ARM)
+          }
+        },
+        "wrist": {
+          "description":"",
+          "mask": motormsk(2, ARM),
+          "verbs": {
+            "open": ARM[2][0],
+            "close": ARM[2][1],
+            "stop": motormsk(2, ARM)
+          }
+        },
+        "grip": {
+          "description":"",
+          "mask": motormsk(3, ARM),
+          "verbs": {
+            "open": ARM[3][1],
+            "close": ARM[3][0],
+            "stop": motormsk(3, ARM)
+          }
+        },
+      }
+    },
+    "light": {
+      "description":"This is an LED mounted behind the <GRIP> feature on the <ARM> component - the focus of the light is useful to highlight where to grab, or that the system is in use",
+      "mask": LED[0],
+      "features": {
+        "switch": {
+          "description":"The only feature of the <LIGHT> is the <SWITCH>, simply flipping the last bit of the 3rd byte",
+          "mask": LED[0],
+          "verbs": {
+            "on": LED[0],
+            "off": LED[1]
+          }
+        }
+      }
+    }
+  }
+}
+
+move_command = [0, 0, 0]
+
+def change_move_command(update_move_command, roboarm_components_system, component, feature, verb):
+  
+  # check that the input is valid against the roboarm_components_system inputs
+  input_exists = check_inputs(roboarm_components_system, component, feature, verb)
+  
+  if input_exists[0]:
+    # when receiving all valid inputs we should be cancelling out the current movement using the mask for the associated level (e.g. feature mask)
+    # this is achieved through the the logical AND(&) with the logical NOT(~) of the mask
+    update_move_command[0] = (update_move_command[0] & ~roboarm_components_system[component]["features"][feature]["mask"][0])
+    update_move_command[1] = (update_move_command[1] & ~roboarm_components_system[component]["features"][feature]["mask"][1])
+    update_move_command[2] = (update_move_command[2] & ~roboarm_components_system[component]["features"][feature]["mask"][2])
+    # the return_move_command is further updated through the logical OR(|) with the desired movement - this leaves the existing values unchanged
+    update_move_command[0] = (update_move_command[0] | roboarm_components_system[component]["features"][feature]["verbs"][verb][0])
+    update_move_command[1] = (update_move_command[1] | roboarm_components_system[component]["features"][feature]["verbs"][verb][1])
+    update_move_command[2] = (update_move_command[2] | roboarm_components_system[component]["features"][feature]["verbs"][verb][2])
+  
+  # calling the function just returns the updated dict {} and doesn't affect any data outside of the function, or use any data that is not passed in
+  return update_move_command, input_exists
+  
+def check_inputs(roboarm_components_system, component, feature, verb):
+  # print(component in roboarm_components_system.keys())
+  component_exists = component in roboarm_components_system.keys()
+  # print(feature in roboarm_components_system[component]["features"].keys())
+  feature_exists = feature in roboarm_components_system[component]["features"].keys()
+  # print(verb in roboarm_components_system[component]["features"][feature]["verbs"].keys())
+  verb_exists = verb in roboarm_components_system[component]["features"][feature]["verbs"].keys()
+  
+  # return the complete list of the 
+  return (component_exists & feature_exists & verb_exists), component_exists, feature_exists, verb_exists
+
+def initialise_roboarm(vendor_id=OWI535USB["identification"]["idVendor"], product_id=OWI535USB["identification"]["idProduct"]):
+  return usb.core.find(idVendor=vendor_id, idProduct=product_id)
+  
+def transfer_roboarm(move_command, ctrl_roboarm=OWI535USB["initialisation"], timeout=1000):
+  # attempt to initialise the device 
+  roboarm_device = initialise_roboarm()
+  
+  # perform the actual USB transfer, returning the length of the transfer
+  transfer_stats = roboarm_device.ctrl_transfer(ctrl_roboarm["bmRequestType"], ctrl_roboarm["bmRequest"], ctrl_roboarm["wValue"], ctrl_roboarm["wIndex"], move_command, timeout)
+  
+  return transfer_stats
+
+app = Bottle()
+
+@app.route('/roboarm/<component>/<feature>/<verb>')
+def move_roboarm(component, feature, verb, move_command=move_command):
+  
+  # print out the input parameters
+  print("request: component={}, feature={}, verb={}".format(component, feature, verb))
+  
+  # print out the current move command
+  print("current move_command:{}".format(move_command))
+  
+  # update the move_command dictionary
+  move_command = change_move_command(move_command, ROVER["components"], component, feature, verb)[0]
+  
+  # print out the updated move command
+  print("new move_command:{}".format(move_command))
+  
+  # make the transfer
+  transfer_attempt = transfer_roboarm(move_command)
+  
+  # print out the result
+  print("transfer_attempt:{}".format(transfer_attempt))
+
+  # return the current move command
+  return move_command[0]
+  
+@app.route('/interface/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root='/home/pi/rover/roboarm/interface')
+
+run(app, host='0.0.0.0', port=8888)
+
+
---
+++ interface/js/index.js
@@ -0,0 +1,537 @@
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var CLICKED_ITEMS = [{
+  component: "",
+  feature: ""
+}, {
+  component: "",
+  action: ""
+}];
+
+var LINKS = [{
+  colorIndex: "graph-2",
+  ids: ["light-switch", "light-off"],
+  component: "light",
+  feature: "switch"
+}, {
+  colorIndex: "graph-2",
+  ids: ["arm-grip", "arm-stop"],
+  component: "arm",
+  feature: "grip"
+}, {
+  colorIndex: "graph-2",
+  ids: ["arm-wrist", "arm-stop"],
+  component: "arm",
+  feature: "wrist"
+}, {
+  colorIndex: "graph-2",
+  ids: ["arm-elbow", "arm-stop"],
+  component: "arm",
+  feature: "elbow"
+}, {
+  colorIndex: "graph-2",
+  ids: ["tracks-both", "tracks-stop"],
+  component: "tracks",
+  feature: "both"
+}];
+
+var ROBOARM = {
+  description: "Giraffe Rover",
+  identification: {
+    description: "These are the parameters used to identify the RoboArm using the USB connection",
+    idVendor: 0x1267,
+    idProduct: 0x000
+  },
+  initialisation: {
+    description: "These variables are used to transfer data on the created USB connection",
+    bmRequestType: 0x40,
+    bmRequest: 6,
+    wValue: 0x100,
+    wIndex: 0
+  },
+  components: {
+    tracks: {
+      description: "The <ARM> components are mounted on this rotating base, with approx. 270deg freedom, clockwise and counterclockwise rotation is considered from the top of the RoboArm",
+      mask: 255,
+      features: {
+        both: {
+          mask: 3,
+          actions: {
+            forward: 1,
+            stop: 2,
+            reverse: 0,
+            turncw: 0,
+            turnccw: 0
+          }
+        }
+      }
+    },
+    arm: {
+      description: "This component is mounted on the <TRACKS>, and replicates a typical human arm - with features, i.e. shoulder (unplugged), elbow, wrist and a gripper.",
+      mask: 255,
+      features: {
+        all: {
+          description: "This is a pseudo-feature that allows for complex functions to be predescribed for the whole <ARM> component",
+          mask: 255,
+          type: "virtual",
+          actions: {
+            open: 170,
+            close: 85,
+            stop: 0
+          }
+        },
+        elbow: {
+          description: "",
+          mask: 48,
+          actions: {
+            open: 16,
+            close: 32,
+            stop: 0
+          }
+        },
+        wrist: {
+          description: "",
+          mask: 12,
+          actions: {
+            open: 4,
+            close: 8,
+            lift: 4,
+            stop: 0
+          }
+        },
+        grip: {
+          description: "",
+          mask: 3,
+          actions: {
+            open: 1,
+            close: 2,
+            grab: 2,
+            drop: 1,
+            stop: 0,
+            pause: 0
+          }
+        }
+      }
+    },
+    light: {
+      description: "This is an LED mounted behind the <GRIP> feature on the <ARM> component - the focus of the light is useful to highlight where to grab, or that the system is in use",
+      mask: 255,
+      features: {
+        switch: {
+          description: "The only feature of the <LIGHT> is the <SWITCH>, simply flipping the last bit of the 3rd byte",
+          mask: 1,
+          actions: {
+            on: 1,
+            off: 0
+          }
+        }
+      }
+    }
+  }
+};
+
+var RoboArmComponents = function (_React$Component) {
+  _inherits(RoboArmComponents, _React$Component);
+
+  function RoboArmComponents() {
+    _classCallCheck(this, RoboArmComponents);
+
+    return _possibleConstructorReturn(this, (RoboArmComponents.__proto__ || Object.getPrototypeOf(RoboArmComponents)).apply(this, arguments));
+  }
+
+  _createClass(RoboArmComponents, [{
+    key: "render",
+    value: function render() {
+      var _this2 = this;
+
+      var componentKeys = Object.keys(this.props.componentConfig);
+      var componentParts = componentKeys.map(function (componentName) {
+        return React.createElement(
+          Topology.Part,
+          {
+            a11yTitle: componentName,
+            className: "component",
+            key: componentName,
+            direction: "row"
+          },
+          React.createElement(RoboArmComponentFeatures, {
+            featureConfig: _this2.props.componentConfig[componentName].features,
+            componentName: componentName,
+            onClick: _this2.props.onClick
+          }),
+          React.createElement(
+            Topology.Label,
+            { className: "raa_component_label" },
+            componentName.toUpperCase()
+          ),
+          React.createElement(RoboArmControlFeatures, {
+            featureConfig: _this2.props.componentConfig[componentName].features,
+            controlName: componentName,
+            onClick: _this2.props.onClick
+          })
+        );
+      });
+      return React.createElement(
+        Topology.Part,
+        {
+          a11yTitle: "RoboArm Component Structure",
+          className: "RoboArmComponents",
+          direction: "column",
+          label: this.props.roboArmName
+        },
+        React.createElement(
+          Topology.Part,
+          _defineProperty({
+            a11yTitle: "RoboArm Component Structure",
+            className: "RoboArmComponents",
+            direction: "column",
+            reverse: true
+          }, "className", "raa_structure"),
+          componentParts
+        )
+      );
+    }
+  }]);
+
+  return RoboArmComponents;
+}(React.Component);
+
+var RoboArmComponentFeatures = function (_React$Component2) {
+  _inherits(RoboArmComponentFeatures, _React$Component2);
+
+  function RoboArmComponentFeatures() {
+    _classCallCheck(this, RoboArmComponentFeatures);
+
+    return _possibleConstructorReturn(this, (RoboArmComponentFeatures.__proto__ || Object.getPrototypeOf(RoboArmComponentFeatures)).apply(this, arguments));
+  }
+
+  _createClass(RoboArmComponentFeatures, [{
+    key: "render",
+    value: function render() {
+      var _this4 = this;
+
+      // const featureRemove = "all";
+      var componentName = this.props.componentName;
+      var featureKeys = Object.keys(this.props.featureConfig);
+      /* let displayFeatureKeys = featureKeys.filter(
+        feature => feature.indexOf(featureRemove) < 0
+      ); */
+      var featureParts = featureKeys.map(function (featureName) {
+        return React.createElement(Topology.Part, {
+          id: componentName + "-" + featureName,
+          status: "ok",
+          label: featureName,
+          direction: "row",
+          demarcate: false,
+          justify: "start",
+          align: "center",
+          key: featureName,
+          onClick: _this4.props.onClick,
+          className: "raa_feature",
+          reverse: true,
+          name: "componentfeature"
+        });
+      });
+      return React.createElement(
+        Topology.Parts,
+        { direction: "column", uniform: true },
+        featureParts
+      );
+    }
+  }]);
+
+  return RoboArmComponentFeatures;
+}(React.Component);
+
+var RoboArmControlFeatures = function (_React$Component3) {
+  _inherits(RoboArmControlFeatures, _React$Component3);
+
+  function RoboArmControlFeatures() {
+    _classCallCheck(this, RoboArmControlFeatures);
+
+    return _possibleConstructorReturn(this, (RoboArmControlFeatures.__proto__ || Object.getPrototypeOf(RoboArmControlFeatures)).apply(this, arguments));
+  }
+
+  _createClass(RoboArmControlFeatures, [{
+    key: "render",
+    value: function render() {
+      var _this6 = this;
+
+      var featureDisplay = "all";
+      var featureKeys = Object.keys(this.props.featureConfig);
+      // console.log(featureKeys.length == 1);
+      var displayFeatureKeys = [];
+      if (featureKeys.length == 1) {
+        displayFeatureKeys = featureKeys;
+      } else {
+        displayFeatureKeys = featureKeys.filter(function (feature) {
+          return feature.indexOf(featureDisplay) > -1;
+        });
+      }
+      // console.log(displayFeatureKeys);
+      var featureParts = displayFeatureKeys.map(function (featureName) {
+        return React.createElement(
+          Topology.Part,
+          {
+            id: featureName,
+            direction: "column",
+            demarcate: false,
+            justify: "start",
+            align: "start",
+            key: featureName
+          },
+          React.createElement(RoboArmControlFeatureActions, {
+            actionConfig: _this6.props.featureConfig[featureName].actions,
+            featureName: featureName,
+            controlName: _this6.props.controlName,
+            onClick: _this6.props.onClick
+          })
+        );
+      });
+      return React.createElement(
+        Topology.Parts,
+        { direction: "column", uniform: true },
+        featureParts
+      );
+    }
+  }]);
+
+  return RoboArmControlFeatures;
+}(React.Component);
+
+var RoboArmControlFeatureActions = function (_React$Component4) {
+  _inherits(RoboArmControlFeatureActions, _React$Component4);
+
+  function RoboArmControlFeatureActions() {
+    _classCallCheck(this, RoboArmControlFeatureActions);
+
+    return _possibleConstructorReturn(this, (RoboArmControlFeatureActions.__proto__ || Object.getPrototypeOf(RoboArmControlFeatureActions)).apply(this, arguments));
+  }
+
+  _createClass(RoboArmControlFeatureActions, [{
+    key: "render",
+    value: function render() {
+      var _this8 = this;
+
+      var featureName = this.props.featureName;
+      var controlName = this.props.controlName;
+      var actionKeys = Object.keys(this.props.actionConfig);
+      var actionParts = actionKeys.map(function (actionName) {
+        return React.createElement(Topology.Part, {
+          id: controlName + "-" + actionName,
+          status: "ok",
+          label: actionName,
+          direction: "row",
+          demarcate: false,
+          justify: "start",
+          align: "center",
+          key: featureName + "-" + actionName,
+          onClick: _this8.props.onClick,
+          className: "raa_action",
+          name: "componentaction"
+        });
+      });
+      return React.createElement(
+        Topology.Parts,
+        { direction: "column", uniform: true },
+        actionParts
+      );
+    }
+  }]);
+
+  return RoboArmControlFeatureActions;
+}(React.Component);
+
+var RoboArmApp = function (_React$Component5) {
+  _inherits(RoboArmApp, _React$Component5);
+
+  function RoboArmApp(props) {
+    _classCallCheck(this, RoboArmApp);
+
+    var _this9 = _possibleConstructorReturn(this, (RoboArmApp.__proto__ || Object.getPrototypeOf(RoboArmApp)).call(this, props));
+
+    _this9.handle_mode_switch = function (event) {
+      console.log(event.target.value);
+    };
+
+    _this9.command_click = function (event) {
+      var clicked_componentfeature = void 0,
+          clicked_componentaction = void 0,
+          cl_parts = void 0,
+          cl_links = void 0;
+
+      cl_parts = _this9.state.clicked_items;
+
+      if (event.target.parentElement.getAttribute("name") == "componentfeature") {
+        clicked_componentfeature = event.target.parentNode.id.split("-");
+        cl_parts[0].component = clicked_componentfeature[0];
+        cl_parts[0].feature = clicked_componentfeature[1];
+        // console.log("clicked a feature: " + cl_parts[0].feature);
+      } else {
+        clicked_componentaction = event.target.parentNode.id.split("-");
+        cl_parts[1].component = clicked_componentaction[0];
+        cl_parts[1].action = clicked_componentaction[1];
+        // console.log("clicked a action: " + cl_parts[1].action);
+      }
+
+      /* hacky bit of hardcoding to make the interface a bit nicer */
+      if (cl_parts[1].component == "light") {
+        cl_parts[0].component = "light";
+        cl_parts[0].feature = "switch";
+      }
+
+      if (cl_parts[1].component == "base") {
+        cl_parts[0].component = "base";
+        cl_parts[0].feature = "orientation";
+      }
+
+      if (cl_parts[0].component == cl_parts[1].component && cl_parts[0].component != "") {
+        _this9.update_links(cl_parts[0].component, cl_parts[0].feature, cl_parts[1].action);
+
+        cl_parts[1].component = "";
+      }
+
+      _this9.setState({
+        clicked_items: cl_parts
+      });
+    };
+
+    _this9.state = {
+      move_command: { arm: 0, base: 0, light: 0 },
+      clicked_items: CLICKED_ITEMS,
+      command_links: LINKS
+    };
+    return _this9;
+  }
+
+  _createClass(RoboArmApp, [{
+    key: "update_links",
+    value: function update_links(component, feature, action) {
+      var updated_link = false;
+      var curr_links = this.state.command_links;
+
+      for (var i = 0; i < curr_links.length; i++) {
+        // console.log("checking: " + i);
+        // console.log("component: " + links[i].component);
+        // console.log("feature: " + links[i].feature);
+        if (curr_links[i].component == component && curr_links[i].feature == feature) {
+          curr_links[i].ids = [component + "-" + feature, component + "-" + action];
+          updated_link = true;
+          // console.log("updating: " + i);
+        }
+      }
+      if (!updated_link) {
+        curr_links.push({
+          colorIndex: "graph-3",
+          ids: [component + "-" + feature, component + "-" + action],
+          component: component,
+          feature: feature
+        });
+      }
+
+      this.setState({
+        command_links: curr_links
+      });
+
+      this.invoke_api(component, feature, action);
+    }
+  }, {
+    key: "invoke_api",
+    value: function invoke_api(component, feature, action) {
+      var api_url = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port;
+      var api_path = "/roboarm/" + component + "/" + feature + "/" + action;
+
+      console.log(api_url + api_path);
+
+      var raa = this;
+
+      fetch(api_url + api_path).then(function (resp) {
+        return resp.json();
+      }).then(function (data) {
+        raa.update_move_command(data);
+      }).catch(function (error) {
+        console.log("ERROR: " + error);
+      });
+    }
+  }, {
+    key: "update_move_command",
+    value: function update_move_command(move_command_response) {
+      this.setState({
+        move_command: move_command_response
+      });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _React$createElement2;
+
+      return React.createElement(
+        App,
+        null,
+        React.createElement(
+          Box,
+          (_React$createElement2 = {
+            align: "center",
+            pad: "medium",
+            colorIndex: "neutral-1",
+            margin: "medium"
+          }, _defineProperty(_React$createElement2, "pad", "small"), _defineProperty(_React$createElement2, "direction", "column"), _React$createElement2),
+          React.createElement(
+            Box,
+            { margin: "medium" },
+            React.createElement(CheckBox, {
+              label: "Live Mode",
+              id: "mode_switch",
+              name: "mode_switch",
+              defaultChecked: true,
+              onChange: this.handle_mode_switch,
+              disabled: true
+            })
+          ),
+          React.createElement(
+            Box,
+            { margin: "medium" },
+            React.createElement("img", { src: "http://giraffe-pi.local/html/cam_pic_new.php?time=1515935947937&pDelay=40000", alt: "Live Feed" })
+          ),
+          React.createElement(
+            Box,
+            { margin: "medium" },
+            React.createElement(
+              Topology,
+              {
+                a11yTitle: this.props.roboarmConfig.description,
+                links: this.state.command_links
+              },
+              React.createElement(
+                Topology.Parts,
+                { direction: "column", uniform: true },
+                React.createElement(
+                  Topology.Part,
+                  { direction: "column" },
+                  React.createElement(RoboArmComponents, {
+                    componentConfig: this.props.roboarmConfig.components,
+                    roboArmName: this.props.roboarmConfig.description,
+                    onClick: this.command_click
+                  })
+                )
+              )
+            )
+          )
+        )
+      );
+    }
+  }]);
+
+  return RoboArmApp;
+}(React.Component);
+
+var element = document.getElementById("content");
+ReactDOM.render(React.createElement(RoboArmApp, { roboarmConfig: ROBOARM }), element);
